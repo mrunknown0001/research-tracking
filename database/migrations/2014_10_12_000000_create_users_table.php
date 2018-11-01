1f512a224e2bb153d25f8099952b5a7c015739c6
Michael Adam <madamt0001@gmail.com>
2018-11-01 09:39:24 +0800

    Initial Commit of the system

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 30);
+            $table->string('lastname', 30);
+            $table->string('id_number')->unique();
+            $table->tinyInteger('user_type');
+            $table->string('password', 80);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
