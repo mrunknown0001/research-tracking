96945da72f548ac61f3b40c64ae6aca4455b63e3
Adam <madamt0001@gmail.com>
2018-11-03 13:24:08 +0800

    Update

--- resources/views/drc/includes/sidebar.blade.php
+++ resources/views/drc/includes/sidebar.blade.php
@@ -1,4 +1,4 @@
-    <div class="sidebar" data-color="danger" data-background-color="black" data-image="">
+    <div class="sidebar" data-color="azure" data-background-color="black" data-image="">
 
       <div class="logo">
         <a href="javascript:void(0)" class="simple-text logo-normal">
@@ -9,11 +9,34 @@
         <ul class="nav">
           <li class="nav-item {{ route('drc.dashboard') == url()->current() ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('drc.dashboard') }}">
-              <i class="material-icons">dashboard</i>
-              <p>Dashboard</p>
+              <i class="material-icons">work</i>
+              <p>Documents</p>
             </a>
           </li>
-
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="material-icons">vertical_align_bottom</i>
+              <p>Incoming Research</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#">
+              <i class="material-icons">vertical_align_top</i>
+              <p>Outgoing Research</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#">
+              <i class="material-icons">file_copy</i>
+              <p>Forms</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#">
+              <i class="material-icons">send</i>
+              <p>Send Request/Form</p>
+            </a>
+          </li>
 
         </ul>
       </div>
