d112427bf5e583f4981e972cf10707b3ba15f02d
Adam <madamt0001@gmail.com>
2018-11-05 08:03:19 +0800

    Added update

--- resources/views/fr/research-incoming.blade.php
+++ resources/views/fr/research-incoming.blade.php
@@ -15,6 +15,35 @@
       <div class="content">
         <div class="container-fluid">
 
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Incoming Research from Research Chairperson </h4>
+              <p class="card-category"> Step 3 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Incoming Research from Research Chairperson </h4>
+              <p class="card-category"> Step 6 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Incoming Research from University Research Office </h4>
+              <p class="card-category"> Step 9 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
 
         </div>
       </div>
--- resources/views/fr/research-outgoing.blade.php
+++ resources/views/fr/research-outgoing.blade.php
@@ -15,6 +15,45 @@
       <div class="content">
         <div class="container-fluid">
 
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Outgoing Research Proceeding to College Research Evaluation Committee </h4>
+              <p class="card-category"> Step 3 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Outgoing Research Proceeding to UREC </h4>
+              <p class="card-category"> Step 6 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Outgoing Research Proceeding to RERC </h4>
+              <p class="card-category"> Step 9 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+
+					<div class="card">
+            <div class="card-header card-header-success">
+              <h4 class="card-title "> Outgoing Research Proceeding to University Research Office </h4>
+              <p class="card-category"> Step 11 </p>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
 
         </div>
       </div>
