66fcc0b2ff875533d70778f0f029ca457de1b843
Adam <webdevsolutions.ph@gmail.com>
2018-11-08 02:29:58 +0800

    Added update on database

--- database/migrations/2018_11_04_192707_create_researches_table.php
+++ database/migrations/2018_11_04_192707_create_researches_table.php
@@ -26,7 +26,52 @@ class CreateResearchesTable extends Migration
             $table->foreign('department_id')->references('id')->on('college_departments');
             $table->tinyInteger('step_number')->default(2);
             $table->timestamp('time_posted')->nullable();
-            $table->tinyInteger('active')->default(1);
+
+            $table->boolean('step_2_received')->default(0);
+            $table->timestamp('step_2_date_received')->nullable();
+            $table->string('step_2_comment', 100)->nullable();
+            $table->boolean('step_2_proceeded')->default(0);
+            $table->timestamp('step_2_date_proceeded')->nullable();
+
+            $table->boolean('step_3_received')->default(0);
+            $table->timestamp('step_3_date_received')->nullable();
+            $table->boolean('step_3_proceeded')->default(0);
+            $table->timestamp('step_3_date_proceeded')->nullable();
+
+            $table->boolean('step_4_received')->default(0);
+            $table->timestamp('step_4_date_received')->nullable();
+            $table->string('step_4_comment', 100)->nullable();
+            $table->boolean('step_4_proceeded')->defaul(0);
+            $table->timestamp('step_4_date_proceeded')->nullable();
+
+            $table->boolean('step_5_received')->default(0);
+            $table->timestamp('step_5_date_received')->nullable();
+            $table->string('step_5_comment', 100)->nullable();
+            $table->tinyInteger('colloquium_grade')->nullable();
+            $table->boolean('step_5_proceeded')->default(0);
+            $table->timestamp('step_5_date_proceeded')->nullable();
+
+            $table->boolean('step_6_received')->default(0);
+            $table->timestamp('step_6_date_received')->nullable();
+            $table->boolean('step_6_proceeded')->default(0);
+            $table->timestamp('step_6_date_proceeded')->nullable();
+
+            $table->boolean('step_7_received')->default(0);
+            $table->timestamp('step_7_date_received')->nullable();
+            $table->string('step_7_comment', 100)->nullable();
+            $table->boolean('step_7_proceeded')->default(0);
+            $table->timestamp('step_7_date_proceeded')->nullable();
+
+            $table->boolean('step_8_received')->default(0);
+            $table->timestamp('step_8_date_received')->nullable();
+            $table->string('step_8_comment', 100)->nullable();
+            $table->tinyInteger('urec_grade')->nullable();
+            $table->integer('agenda_id')->unsigned()->nullable();
+            $table->foreign('agenda_id')->references('id')->on('agendas');
+            $table->boolean('step_8_proceeded')->default(0);
+            $table->timestamp('step_8_date_proceeded')->nullable();
+
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
